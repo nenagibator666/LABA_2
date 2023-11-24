7c440bb6fab14c591485166e081b75c2fab3c357
nenagibator666 <128630208+nenagibator666@users.noreply.github.com>
2023-11-24 17:31:39 +0300

    Add files via upload

---
+++ Archives/SampleText/4PP065_0571_P74/libDriveLib.c
@@ -0,0 +1,6 @@
+void DriveStateMachine(void) {};
+void _DriveStateMachine(void) {};
+void LedStateMachine(void) {};
+void _LedStateMachine(void) {};
+void DoorStateMachine(void) {};
+void _DoorStateMachine(void) {};
